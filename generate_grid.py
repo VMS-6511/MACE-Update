f6dc4c01987504cbcf60ea418a1fed46cdb7a415
Rohan Alur <ralur@mit.edu>
2024-09-27 15:35:57 -0400

    qualitative examples

--- generate_grid.py
+++ generate_grid.py
@@ -4,11 +4,21 @@ import glob
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-
+import argparse
 
 if __name__ == "__main__":
-    directories = ["/data/healthy-ml/scratch/ralur/projects/MACE-Update/experiments/MACE/", "/data/healthy-ml/scratch/vinithms/projects/MACE-Update/experiments/MACE/"]
-    prompt = 'A portrait of Adam Driver'
+    #directories = ["/data/healthy-ml/scratch/ralur/projects/MACE-Update/experiments/MACE/", "/data/healthy-ml/scratch/vinithms/projects/MACE-Update/experiments/MACE/"]
+    directories = ["/data/healthy-ml/scratch/vinithms/projects/MACE-Update/experiments/MACE/"]
+
+    parser = argparse.ArgumentParser(description='Generate image grid')
+    parser.add_argument('--prompt', type=str, default='A portrait of Adam Driver',
+                        help='Prompt for image generation')
+    parser.add_argument('--change', type=str, default='baseline',
+                        help='Name of change')
+    args = parser.parse_args()
+
+    prompt = args.prompt
+    change = args.change
 
     def find_images(base_path, prompt):
         images = []
@@ -19,14 +29,21 @@ if __name__ == "__main__":
                     
         return images
 
-    def generate_image_grid(directories, prompt):
+    def generate_image_grid(directories, prompt, change):
         all_experiments = []
         for directory in directories:
-            for experiment in os.listdir(directory):
-                experiment_path = os.path.join(directory, experiment)
-                if os.path.isdir(experiment_path):
-                    all_experiments.append(experiment_path)
-
+            for subdir in os.listdir(directory):
+                if subdir.startswith(change):
+                    print('Found experiment: ', subdir)
+                    all_experiments.append(os.path.join(directory, subdir))
+                    
+                    # for experiment in os.listdir(os.path.join(directory, subdir)):
+                    #     experiment_path = os.path.join(subdir, experiment)
+                    #     if os.path.isdir(experiment_path):
+                    #         print('Found experiment: ', experiment_path)
+                    #         all_experiments.append(experiment_path)
+                   
+            
         valid_experiments = []
         for experiment_path in all_experiments:
             original_images = find_images(os.path.join(experiment_path, '0', 'inference'), prompt)
@@ -57,8 +74,10 @@ if __name__ == "__main__":
             axes[i, 0].text(0.5, -0.1, experiment_name, size=12, ha="center", transform=axes[i, 0].transAxes)
 
         plt.tight_layout()
-        output_file = 'image_grid.png'
+        output_dir = os.path.join('figures', change)
+        os.makedirs(output_dir, exist_ok=True)
+        output_file = os.path.join(output_dir, f'{prompt}.png')
         plt.savefig(output_file)
         print(f"Image grid saved to {output_file}")
 
-    generate_image_grid(directories, prompt)
+    generate_image_grid(directories, prompt, change)
